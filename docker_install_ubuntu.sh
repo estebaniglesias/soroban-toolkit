c7d7df72d686dc4080546294b8aeaa67118bbf32
estebaniglesias <estebaniglesiasmanriquez@gmail.com>
2022-10-14 15:26:42 +0200

    install docker instructions

---
+++ docker_install_ubuntu.sh
@@ -0,0 +1,27 @@
+# Following instructions in https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#docker-ce | 5:20.10.19~3-0~ubuntu-jammy | https://download.docker.com/linux/ubuntu jammy/stable amd64 Packages
+
+sudo apt-get install \
+  docker-ce=5:20.10.19~3-0~ubuntu-jammy \
+  docker-ce-cli=5:20.10.19~3-0~ubuntu-jammy \
+  containerd.io \
+  docker-compose-plugin
